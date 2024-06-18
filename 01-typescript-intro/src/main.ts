d7ca9fc8161dc1ee5b84d700b72bbaba9aa7c842
jota santos <gestald118@email.com>
2024-06-18 08:11:15 -0400

    19. Tarea sobre objetos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './topics/03-functions';
+// import './topics/03-functions';
+import './topics/04-homework-type';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hello World Angular 
