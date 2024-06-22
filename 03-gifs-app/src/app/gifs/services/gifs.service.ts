0fe5f306ffa4c9fcbf0136155d9a582dc98b9e7e
jota santos <gestald118@email.com>
2024-06-22 07:34:12 -0400

    gravando no localstorage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -57,4 +57,9 @@ export class GifsService {
     this._tagsHistory = this.tagsHistory.splice(0,10);
   }
 
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+    this.saveLocalStorage();
+  }
+
 }
