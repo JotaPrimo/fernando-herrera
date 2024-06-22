d97973282c00b48027e828028a3222349f1438bf
jota santos <gestald118@email.com>
2024-06-22 15:10:43 -0400

    123. Countries Service - Restcountries

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    
+    HttpClientModule,
+
     SharedModule
   ],
   providers: [],
--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Country } from '../interfaces/country';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  private url = 'https://restcountries.com/v3.1';
 
-constructor() { }
+  constructor(private http: HttpClient) {}
+
+
+  public searchCapital(term: string):Observable<Country[]> {
+    const url = `${this.url}/capital/${term}`;
+    return this.http.get<Country[]>( url );
+  }
+
+
 
 }
