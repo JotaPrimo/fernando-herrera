4959b1e8b2c1bfe1c4c70afc6502130dcbd2050a
jota santos <gestald118@email.com>
2024-06-23 13:17:22 -0400

    154. PrimeNg - MenuBar

--- 05-pipesApp/src/app/shared/components/menu/menu.component.ts
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -4,22 +4,32 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-
   public menuItems: MenuItem[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.menuItems = [
-      {label: 'Home', icon: 'pi pi-fw pi-home'},
-      {label: 'Open', icon: 'pi pi-fw pi-download'},
-      {label: 'Undo', icon: 'pi pi-fw pi-refresh'},
+      {
+        label: 'Textos e flechas',
+        icon: 'pi pi-fw pi-home',
+        items: [
+          { label: 'Número', icon: 'pi pi-fw pi-plus' },
+          { label: 'Não comunes', icon: 'pi pi-fw pi-globe' },
+        ],
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro personalizado',
+          }
+        ]
+      }
     ];
   }
-
-
-
 }
--- 05-pipesApp/src/app/shared/shared.module.ts
+++ 05-pipesApp/src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { MenuComponent } from './components/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from '../primeNg/primeNg.module';
 
+
+import { MenubarModule } from 'primeng/menubar';
+
 @NgModule({
   imports: [
     CommonModule,
