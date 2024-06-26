fc17cb885507b0d511f6ec29495d1cea20601837
Jota Santos <jailsonsantosdpge@outlook.com>
2024-06-26 12:36:15 -0400

    limpando tudo e recomemeçando

--- 06-heroesApp/src/app/auth/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './layout-page/layout-page.component';
-export * from './login-page/login-page.component';
-export * from './register-page/register-page.component';
--- 06-heroesApp/src/app/heroes/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './hero-page/hero-page.component';
-export * from './layout-page/layout-page.component';
-export * from './list-page/list-page.component';
-export * from './new-page/new-page.component';
-export * from './search-page/search-page.component';
